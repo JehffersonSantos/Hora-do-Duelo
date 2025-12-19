789493e774e910a5f7d686407f95216ced769e5f
JehffersonSantos <jefersonsantos10@hotmail.com>
2025-12-18 21:22:56 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const escolherCarta = document.querySelector('.Jogar_Carta')
+const carta_Mao = document.querySelectorAll('.Carta')
+
+carta_Mao.forEach(element => {
+    
+});
+
+escolherCarta.addEventListener('click', (e) => {
+    console.log('clicou')
+})
